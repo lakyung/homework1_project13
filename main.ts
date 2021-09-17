c2cea8074c6b51ad2eba9b4eb9bb3d90d9f48205
lakyung <90668130+lakyung@users.noreply.github.com>
2021-09-17 13:13:52 +0900

    a버튼에 소리나기

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 basic.forever(function () {
     if (tinkercademy.PIR(DigitalPin.P1)) {
         music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
